502e4233d98b1b2a6f7c365037acb03713a87f3b
Yebai <704535747@qq.com>
2025-05-26 17:12:35 +0800

    Add function save_point_cloud_as_ply

--- depth_and_point_cloud_related.py
+++ depth_and_point_cloud_related.py
@@ -1,1 +1,35 @@
+import numpy as np
 
+def save_point_cloud_as_ply(save_path, pcd, rgb=None):
+
+    pcd = pcd.reshape((-1,3))
+    if rgb is not None:
+        rgb = rgb.reshape((-1,3))
+        
+    header = (
+        'ply\n'
+        'format ascii 1.0\n'
+        f'element vertex {len(pcd)}\n'
+        'property float x\n'
+        'property float y\n'
+        'property float z\n'
+    )
+    
+    if rgb is not None:
+        header += (
+            'property uint8 red\n'
+            'property uint8 green\n'
+            'property uint8 blue\n'
+        )
+        
+    header += 'end_header\n'
+
+    with open(save_path, 'w') as f:
+        
+        f.write(header)
+        
+        if rgb is None:
+            np.savetxt(f, pcd, '%f %f %f', delimiter=' ')
+        else:
+            tmp = np.concatenate((pcd, rgb), axis=-1)
+            np.savetxt(f, tmp, '%f %f %f %d %d %d', delimiter=' ')

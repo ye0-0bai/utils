8f549b30abc87532cb277ca90d240b8015a94bf1
Yebai <704535747@qq.com>
2025-06-05 12:02:10 +0800

    Add sort_key argument to func read_images

--- image_and_video_related.py
+++ image_and_video_related.py
@@ -1,12 +1,12 @@
 import numpy as np
 import imageio.v3 as iio
 
-def read_images(image_dir):
+def read_images(image_dir, sort_key=None):
     image_names = []
     for tmp in os.listdir(image_dir):
         if tmp.endswith(('jpg', 'png')):
             image_names.append(tmp)
-    image_names = sorted(image_names)
+    image_names.sort(key=sort_key)
     images = []
     for image_name in image_names:
         images.append(iio.imread(os.path.join(image_dir, image_name)))

bf694adf57fb13fc4a1398150de2e2dbb33a4ebc
Yebai <704535747@qq.com>
2025-06-09 20:38:39 +0800

    Add func convert_depth_to_point_cloud

--- depth_and_point_cloud_related.py
+++ depth_and_point_cloud_related.py
@@ -38,3 +38,41 @@ def save_point_cloud_as_ply(save_path:str, pcd:np.ndarray, rgb:np.ndarray=None) 
         else:
             tmp = np.concatenate((pcd, rgb), axis=-1)
             np.savetxt(f, tmp, '%f %f %f %d %d %d', delimiter=' ')
+
+def convert_depth_to_point_cloud(depth:np.ndarray, intrinsic:np.ndarray, scale:int=1) -> np.ndarray:
+    """Convert depth image(s) to 3D point cloud(s) using camera intrinsics.
+
+    Args:
+        depth (np.ndarray)
+        intrinsic (np.ndarray)
+        scale (int, optional): Scaling factor to convert depth values to meters. Defaults to 1.
+
+    Returns:
+        np.ndarray: point cloud(s)
+    """
+    
+    batched_input = True
+    
+    if depth.ndim == 2:
+        batched_input = False
+        depth = depth[None,...]
+        intrinsic = intrinsic[None,...]
+        
+    H, W = depth.shape[1], depth.shape[2]
+    u,v = np.meshgrid(np.arange(W), np.arange(H), indexing='xy')
+    
+    u = u.astype(np.float32)
+    v = v.astype(np.float32)
+    depth = depth.astype(np.float32)
+    intrinsic = intrinsic.astype(np.float32)
+    
+    Z = depth / scale
+    X = (u[None,...] - intrinsic[:,0,2][:,None,None]) * Z / intrinsic[:,0,0][:,None,None]
+    Y = (v[None,...] - intrinsic[:,1,2][:,None,None]) * Z / intrinsic[:,1,1][:,None,None]
+    
+    point_clouds = np.stack((X, Y, Z), axis=-1)
+    
+    if batched_input:
+        return point_clouds
+    else:
+        return point_clouds[0]
